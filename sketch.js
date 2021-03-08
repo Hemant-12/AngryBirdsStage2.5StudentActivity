c70752efb10f1231d1fa829ebac85f20d4b9ffe8
Hemant-12 <76694585+Hemant-12@users.noreply.github.com>
2021-03-08 20:53:27 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -17,8 +17,9 @@ function setup(){
 
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
-
+    platform1= new Ground(150, 305, 300, 170);
+    platform2 = new Ground(100,305,300,170);
+    
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -60,5 +61,6 @@ function draw(){
     log5.display();
 
     bird.display();
-    platform.display();
+    platform1.display();
+    platform2.display()
 }
